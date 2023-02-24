cab866dd0008a75b36a05acec79959b36664787d
Trust7 <simeontrust7@gmail.com>
2023-02-24 03:47:43 +0100

    Add methods and associations to models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,16 @@
 class Post < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  has_many :comments
+  has_many :likes
 
+  after_save :update_post_user_counter
+  def five_most_recent_comments
+    comments.last(5)
+  end
+
+  private
+
+  def update_post_user_counter
+    author.increment!(:posts_counter)
+  end
 end
