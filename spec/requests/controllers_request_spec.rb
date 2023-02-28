a689ee34652ffe7b287edd73c523bb0dcf355992
Trust7 <simeontrust7@gmail.com>
2023-02-28 20:36:25 +0100

    Fixed linter errors

--- spec/requests/controllers_request_spec.rb
+++ spec/requests/controllers_request_spec.rb
@@ -1,56 +1,56 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.describe "Users Controller", type: :request do
-    context 'GET #index for users' do
-        before(:example) { get users_path }
-        it "is a success" do
-            expect(response).to have_http_status(:ok)
-        end
-        it "renders 'index' template" do
-            expect(response).to render_template('index')
-        end
-        it "includes placeholder text" do
-            expect(response.body).to include('Here is a list of users')
-        end
+RSpec.describe 'Users Controller', type: :request do
+  context 'GET #index for users' do
+    before(:example) { get users_path }
+    it 'is a success' do
+      expect(response).to have_http_status(:ok)
     end
-    context 'Get #show for user' do
-        before(:example) { get user_path(id: 1) }
-        it "is a success" do
-            expect(response).to have_http_status(:ok)
-        end
-        it "renders 'index' template" do
-            expect(response).to render_template('show')
-        end
-        it "includes placeholder text" do
-            expect(response.body).to include('Here is a single user')
-        end
+    it "renders 'index' template" do
+      expect(response).to render_template('index')
     end
+    it 'includes placeholder text' do
+      expect(response.body).to include('Here is a list of users')
+    end
+  end
+  context 'Get #show for user' do
+    before(:example) { get user_path(id: 1) }
+    it 'is a success' do
+      expect(response).to have_http_status(:ok)
+    end
+    it "renders 'index' template" do
+      expect(response).to render_template('show')
+    end
+    it 'includes placeholder text' do
+      expect(response.body).to include('Here is a single user')
+    end
+  end
 end
 
-RSpec.describe "Posts Controller", type: :request do
-     context 'GET #index for user_posts' do
-        before(:example) { get user_posts_path(user_id: 1) }
-        it "is a success" do
-            expect(response).to have_http_status(:ok)
-        end
-        it "renders 'index' template" do
-            expect(response).to render_template('index')
-        end
-        it "includes placeholder text" do
-            expect(response.body).to include('Here is a list of posts for a given user')
-        end
+RSpec.describe 'Posts Controller', type: :request do
+  context 'GET #index for user_posts' do
+    before(:example) { get user_posts_path(user_id: 1) }
+    it 'is a success' do
+      expect(response).to have_http_status(:ok)
     end
+    it "renders 'index' template" do
+      expect(response).to render_template('index')
+    end
+    it 'includes placeholder text' do
+      expect(response.body).to include('Here is a list of posts for a given user')
+    end
+  end
 
-    context 'GET #show for user_post' do
-        before(:example) { get user_post_path(user_id: 1, id: 1) }
-        it "is a success" do
-            expect(response).to have_http_status(:ok)
-        end
-        it "renders 'index' template" do
-            expect(response).to render_template('show')
-        end
-        it "includes placeholder text" do
-            expect(response.body).to include('Here is a single post for a given user')
-        end
+  context 'GET #show for user_post' do
+    before(:example) { get user_post_path(user_id: 1, id: 1) }
+    it 'is a success' do
+      expect(response).to have_http_status(:ok)
     end
+    it "renders 'index' template" do
+      expect(response).to render_template('show')
+    end
+    it 'includes placeholder text' do
+      expect(response.body).to include('Here is a single post for a given user')
+    end
+  end
 end
