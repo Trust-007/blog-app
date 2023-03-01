75e9f9af09bbb033e32602550336e52dd3cb7478
Trust7 <simeontrust7@gmail.com>
2023-03-01 22:46:57 +0100

    Add dynamic views for posts_controller index and show actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,12 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
+    @posts = Post.where(params[:author_id])
+    @user = User.find(params[:user_id])
+    @comments = Comment.where(params[:user_post_id])
+  end
 
-  def show; end
+  def show
+    @post = Post.find(params[:id])
+    @comments = Comment.where(params[:comment_post_id])
+  end
 end
