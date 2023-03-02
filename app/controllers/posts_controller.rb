15eff27dd1a1ca457f65fbaf619088d07c5be1d4
Trust7 <simeontrust7@gmail.com>
2023-03-02 12:03:22 +0100

    Removed comment getter in posts_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,6 @@ class PostsController < ApplicationController
   def index
     @posts = Post.where(params[:author_id])
     @user = User.find(params[:user_id])
-    @comments = @posts.each { |post| post.five_most_recent_comments}
   end
 
   def show
